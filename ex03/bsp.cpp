e8cf004a6a30a47fcbb5b42a64e3967f2d14632d
JC-Palmeraie <epinaud@student.42.fr>
2025-08-26 19:39:32 +0200

    Fixed the erroneous computation in the binary space partitioning formula

--- ex03/bsp.cpp
+++ ex03/bsp.cpp
@@ -6,19 +6,18 @@
 /*   By: epinaud <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/30 00:06:39 by epinaud           #+#    #+#             */
-/*   Updated: 2025/08/20 23:44:29 by epinaud          ###   ########.fr       */
+/*   Updated: 2025/08/26 19:35:32 by epinaud          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Fixed.hpp"
 #include "Point.hpp"
 
-
 //sign[edge] = AP.x * BA.y - BA.x * AP.y
 //(true if on the left of edge AB, false if overlaping or positioned to the right)
 //Optional method :  AP.X * BP.Y - BP.X * AP.Y
 static Fixed	isLeftOfBsp(Point a, Point b, Point pt) {
-	return (a.getX() - pt.getX()) * (b.getY() - a.getY()) - (b.getX() - a.getX()) * (a.getY() - b.getY());
+	return (a.getX() - pt.getX()) * (b.getY() - a.getY()) - (b.getX() - a.getX()) * (a.getY() - pt.getY());
 }
 
 bool bsp( Point const a, Point const b, Point const c, Point const point) {
